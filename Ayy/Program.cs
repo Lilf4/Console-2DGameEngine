367b15dd74971cf9d1c1567e7e901e3980d28797
Danfa <danfa.danfa@hotmail.com>
2021-05-06 14:37:37 +0200

    kkk

--- Ayy/Program.cs
+++ Ayy/Program.cs
@@ -7,39 +7,16 @@ namespace Ayy
     class Program
     {
         public static int Width = 100;
-        public static int Height = 100;
-        static DrawPoint[,] WhiteSreen = new DrawPoint[Width, Height];
+        public static int Height = 40;
         static void Main(string[] args)
         {
             DrawPoint[,] DisplayBuffer = DrawPoint.InitPoint(Width, Height);
-            for(int y = 0; y < Height; y++)
-            {
-                for(int x = 0; x < Width; x++)
-                {
-                    DisplayBuffer[x, y].DisplayChar = '#';
-                    DisplayBuffer[x, y].DisplayColor = ConsoleColor.White;
-                }
-            }
-            WhiteSreen = DrawPoint.Copy(DisplayBuffer);
-           
+            DisplayBuffer = DrawPoint.Const('#', ConsoleColor.White, DisplayBuffer);
             Draw ConsoleDrawer = new Draw(10, Width, Height, DisplayBuffer, "Yeeetumus");
             ConsoleDrawer.Start();
-            Random Ran = new Random();
-            while (true)
-            {
-                for(int i = 0; i < 100; i++)
-                {
-                    int x = Ran.Next(0, Width);
-                    int y = Ran.Next(0, Height);
-                    if (DisplayBuffer[x, y].DisplayColor == ConsoleColor.Black)
-                    { i--; continue; }
-                    DisplayBuffer[x, y].DisplayColor = ConsoleColor.Black;
-                    
-                }
-                ConsoleDrawer.UpdateBuffer(DisplayBuffer);
-                Thread.Sleep(1000);
-                DisplayBuffer = DrawPoint.Copy(WhiteSreen);
-            }
+            Thread.Sleep(1000);
+            DisplayBuffer = DrawPoint.Const('#', ConsoleColor.DarkGreen, DisplayBuffer);
+            ConsoleDrawer.UpdateBuffer(DisplayBuffer);
         }
 
         
@@ -49,20 +26,18 @@ namespace Ayy
         public char DisplayChar = ' ';
         public ConsoleColor DisplayColor = ConsoleColor.White;
 
-        public static DrawPoint[,] Copy(DrawPoint[,] ObjToCopy)
+        public static DrawPoint[,] CopyTo(DrawPoint[,] ObjToCopy, DrawPoint[,] ObjToTransferTo)
         {
-            DrawPoint[,] A = new DrawPoint[ObjToCopy.GetLength(0), ObjToCopy.GetLength(1)];
-            
             for (int y = 0; y < ObjToCopy.GetLength(1); y++)
             {
                 for (int x = 0; x < ObjToCopy.GetLength(0); x++)
                 {
-                    A[x, y] = new DrawPoint();
-                    A[x, y].DisplayChar = ObjToCopy[x, y].DisplayChar;
-                    A[x, y].DisplayColor = ObjToCopy[x, y].DisplayColor;
+                    if (ObjToTransferTo[x, y] == null) { ObjToTransferTo[x, y] = new DrawPoint(); }
+                    ObjToTransferTo[x, y].DisplayChar = ObjToCopy[x, y].DisplayChar;
+                    ObjToTransferTo[x, y].DisplayColor = ObjToCopy[x, y].DisplayColor;
                 }
             }
-            return A;
+            return ObjToTransferTo;
         }
 
         public static DrawPoint[,] InitPoint(int width, int height)
@@ -77,11 +52,25 @@ namespace Ayy
             }
             return Out;
         }
+
+        public static DrawPoint[,] Const(char Char, ConsoleColor Color, DrawPoint[,] Buffer)
+        {
+            for(int y = 0; y < Buffer.GetLength(1); y++) 
+            {
+                for(int x = 0; x < Buffer.GetLength(0); x++)
+                {
+                    if (Buffer[x, y] == null) { Buffer[x, y] = new DrawPoint(); }
+                    Buffer[x, y].DisplayChar = Char;
+                    Buffer[x, y].DisplayColor = Color;
+                }
+            }
+            return Buffer;
+        }
     }
 
     class Draw
     {
-        bool ForceDraw;
+        public bool ForceDraw;
         int Width;
         int Height;
         int TimeBetweenUpdates;
@@ -90,23 +79,27 @@ namespace Ayy
         DrawPoint[,] Buffer;
         /// <summary>
         /// Initiates a draw object
-        /// 
         /// </summary>
         /// <param name="TimeBetweenUpdates">Time in miliseconds between each frame update</param>
+        /// <param name="width">Width of display</param>
+        /// <param name="height">Height of display</param>
+        /// <param name="StartBuffer">First buffer screen to display</param>
+        /// <param name="Title">Title of console program</param>
         public Draw(int TimeBetweenUpdates, int width, int height, DrawPoint[,] StartBuffer, string Title)
         {
             this.Width = width;
             this.Height = height;
             this.TimeBetweenUpdates = TimeBetweenUpdates;
-            this.CurrDisplay = StartBuffer;
-            this.Buffer = StartBuffer;
+            this.CurrDisplay = DrawPoint.Const(' ', ConsoleColor.White, new DrawPoint[width, height]);
+            this.Buffer = DrawPoint.CopyTo(StartBuffer, new DrawPoint[width, height]);
             if (Console.BufferHeight < Height) { Console.BufferHeight = Height; }
             if (Console.BufferWidth < Width) { Console.BufferWidth = Width; }
             Console.Title = Title;
+            Console.WindowHeight = Height;
+            Console.WindowWidth = Width;
         }
         /// <summary>
-        /// Updates the display buffer with a new one
-        /// This is technically just the same as directly changing the Buffer value itself
+        /// Updates the display buffer
         /// </summary>
         /// <param name="Buffer">stuff to display on screen</param>
         public void UpdateBuffer (DrawPoint[,] Buffer)
@@ -143,7 +136,7 @@ namespace Ayy
             {
                 for (int x = 0; x < Width; x++)
                 {
-                    if (Buffer[x, y].DisplayChar != CurrDisplay[x, y].DisplayChar || Buffer[x, y].DisplayColor != CurrDisplay[x, y].DisplayColor || ForceDraw)
+                    if ((Buffer[x, y].DisplayChar != CurrDisplay[x, y].DisplayChar) || (Buffer[x, y].DisplayColor != CurrDisplay[x, y].DisplayColor) || ForceDraw)
                     {
                         Console.SetCursorPosition(x, y);
                         Console.ForegroundColor = Buffer[x, y].DisplayColor;
@@ -153,7 +146,7 @@ namespace Ayy
                 }
             }
             ForceDraw = false;
-            CurrDisplay = Buffer;
+            CurrDisplay = DrawPoint.CopyTo(Buffer, CurrDisplay);
         }
     }
 }
